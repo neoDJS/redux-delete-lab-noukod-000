3f90245bb6804417f0ec69d3abca3cbbddde87a6
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-11 07:00:27 +0000

    Automatically backed up by Learn

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -10,7 +10,7 @@ export default function manageBand(state = {
       return { ...state, bands: [...state.bands, band] }
     case 'ADD_BAND':
 
-      return {bands: state.bands.filter(band => band.id !== action.id)}
+      return {bands: state.bands.filter(band => band.id !== action.id)};
 
     default:
       return state;
