290ace5eb59c2fa71f1277df7132357a6f63310b
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-11 06:59:02 +0000

    Automatically backed up by Learn

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -3,7 +3,10 @@ export default function manageBand(state = {
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
-
+      band = {
+        id:  Math.random()*10000000000000000,
+        name: action.name
+      }
       return { ...state, bands: [...state.bands, action.name] }
 
     default:
