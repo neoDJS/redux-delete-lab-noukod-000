c03edd14f621ef3ec776f33b2d91f85f7f11df16
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-11 06:48:30 +0000

    Automatically backed up by Learn

--- src/components/BandsContainer.js
+++ src/components/BandsContainer.js
@@ -4,11 +4,13 @@ import BandInput from './BandInput';
 import { connect } from 'react-redux'
 
 class BandsContainer extends Component {
+
+  renderBands = () => this.props.bands.map(band => <Todo key={band.id} band={band} />)
   render() {
     return (
       <div>
         <BandInput addBand={this.props.addBand}/>
-
+        {this.renderBands()}
       </div>
     )
   }
